2d6b7879c013b7457036931551a5a45027d55ed5
Anastasija <forever131097@gmail.com>
2024-11-14 21:02:59 +0200

    [featureTWP-1347]

---
+++ src/main/java/lv/acodemy/loops.java
@@ -0,0 +1,37 @@
+package lv.acodemy;
+
+import java.;
+
+public class loops {
+    public static void main(String[] args) {
+        String[] shoppinglist = {"Milk", "Eggs", "Cola", "Fruits"};
+        for (int i = 0; i < shoppingList.length; i++) {
+            System.out.println("Current product is " + shoppinglist[i]);
+        }
+        System.out.println("Goes next!");
+        String[] shoppingList = {"Milk", "Eggs", "Cola", "Fruits"};
+        for (int i = 0; i < shoppinglist.length; i++) {
+            System.out.println("Current product is " + shoppinglist[i]);
+        }
+        System.out.println("Goes next!");
+        int i= 0
+                while(i < shoppingList.length) {
+                    System.out.println("Current product is " +shoppingList[i]);
+                    i++;
+                }
+                    int j=0;
+                do {
+                    System.out.println("Current product is " + shoopingList[j]);
+                    j++;
+                } while (j < shoopinglist.lengt);
+
+                boolean x = 1 <5 && 6 < 3 && 5 < 3 ; // AND
+        System.out.println(x);
+
+
+
+
+
+    }
+
+}
